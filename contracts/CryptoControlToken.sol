dc63835d0357bb0e025d58dc391e16764026bc61
Steven Enamakel <enamakel@cryptocontrol.io>
2018-08-24 03:03:51 +0530

    added cryptocontrol token

---
+++ contracts/CryptoControlToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.22;
+
+import 'openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/BasicToken.sol';
+
+
+contract CryptoControlToken is DetailedERC20, BasicToken {
+
+}
