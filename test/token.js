f140121a6b208392f8112f67b8b80c2a946f3012
Steven Enamakel <enamakel@cryptocontrol.io>
2018-08-24 03:36:27 +0530

    mionr change in unit test

--- test/token.js
+++ test/token.js
@@ -22,27 +22,27 @@ contract('CryptoControlToken', function (accounts) {
     })
 
 
-    it('prevent suicideContract() by a non-owner', async function () {
-        // const instance = await Suicidable.deployed()
+    // it('prevent suicideContract() by a non-owner', async function () {
+    //     // const instance = await Suicidable.deployed()
 
-        // // attempt to suicide the contract
-        // await expectRevert(this.suicidable.suicideContract({
-        //     from: accounts[1]
-        // }))
-    })
+    //     // // attempt to suicide the contract
+    //     // await expectRevert(this.suicidable.suicideContract({
+    //     //     from: accounts[1]
+    //     // }))
+    // })
 
 
-    it('allow suicideContract() by a previous owner', async function () {
-        // // Transfer ownership to account[1]
-        // await this.suicidable.transferOwnership(accounts[1])
+    // it('allow suicideContract() by a previous owner', async function () {
+    //     // // Transfer ownership to account[1]
+    //     // await this.suicidable.transferOwnership(accounts[1])
 
-        // // attempt to suicide the contract
-        // this.suicidable.suicideContract({
-        //     from: accounts[0]
-        // })
+    //     // // attempt to suicide the contract
+    //     // this.suicidable.suicideContract({
+    //     //     from: accounts[0]
+    //     // })
 
-        // // check the suicide flag
-        // const hasSuicided = await this.suicidable.hasSuicided()
-        // assert.equal(hasSuicided, true)
-    })
+    //     // // check the suicide flag
+    //     // const hasSuicided = await this.suicidable.hasSuicided()
+    //     // assert.equal(hasSuicided, true)
+    // })
 })
